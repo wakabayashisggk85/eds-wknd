accfe959d3489ec4b666c58c7787a89dca048f92
Marco Pasini <mpasini@adobe.com>
2025-02-14 17:28:36 +0100

    wknd styled

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,20 +1,28 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
- * @param {Element} block The footer block element
+ * @param {Element} block The header block element
  */
+
 export default async function decorate(block) {
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+  const navPath = window.wknd.demoConfig.demoBase || '';
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  const resp = await fetch(`${navPath}/footer.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  if (resp.ok) {
+    block.textContent = '';
 
-  block.append(footer);
+    const html = await resp.text();
+    const footer = document.createElement('div');
+    footer.innerHTML = html;
+    await decorateIcons(footer);
+
+    const classes = ['brand', 'nav', 'follow', 'disc'];
+    let f = footer.firstElementChild;
+    while (f && classes.length) {
+      f.classList.add(classes.shift());
+      f = f.nextElementSibling;
+    }
+    block.append(footer);
+  }
 }
